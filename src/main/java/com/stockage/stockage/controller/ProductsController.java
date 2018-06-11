29ff68c4b8a0eb02a3e5c9c6c5987b202f97a45d
Santiago Correa <scorrea153@gmail.com>
2018-06-11 20:58:04 +0200

    Permitir descargar un pdf con el stock disponible

--- src/main/java/com/stockage/stockage/controller/ProductsController.java
+++ src/main/java/com/stockage/stockage/controller/ProductsController.java
@@ -4,14 +4,20 @@ import com.stockage.stockage.exception.ResourceNotFoundException;
 import com.stockage.stockage.model.locations;
 import com.stockage.stockage.model.products;
 import com.stockage.stockage.model.providers;
+import com.stockage.stockage.reports.Reports;
 import com.stockage.stockage.repository.LocationsRepository;
 import com.stockage.stockage.repository.ProductsRepository;
 import com.stockage.stockage.repository.ProvidersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -79,5 +85,20 @@ public class ProductsController {
         productsRepository.delete(p);
 
         return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value = "/stockreport", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> testReport() throws IOException {
+
+        ByteArrayInputStream bis = Reports.stockReport(getAllProducts());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=test.pdf");
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(bis));
     }
 }
---
+++ src/main/java/com/stockage/stockage/reports/Reports.java
@@ -0,0 +1,74 @@
+package com.stockage.stockage.reports;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.stockage.stockage.model.products;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class Reports {
+
+    public static ByteArrayInputStream stockReport(java.util.List<products> allProducts) {
+
+        Document document = new Document();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        try {
+
+            PdfPTable table = new PdfPTable(3);
+            table.setWidthPercentage(60);
+            table.setWidths(new int[]{1, 3, 3});
+
+            Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+
+            PdfPCell hcell;
+            hcell = new PdfPCell(new Phrase("Id", headFont));
+            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(hcell);
+
+            hcell = new PdfPCell(new Phrase("Name", headFont));
+            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(hcell);
+
+            hcell = new PdfPCell(new Phrase("Stock", headFont));
+            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(hcell);
+
+            for (products p : allProducts) {
+
+                PdfPCell cell;
+
+                cell = new PdfPCell(new Phrase(p.getProducts_id().toString()));
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                table.addCell(cell);
+
+                cell = new PdfPCell(new Phrase(p.getProducts_name().toString()));
+                cell.setPaddingLeft(5);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+                table.addCell(cell);
+
+                cell = new PdfPCell(new Phrase(String.valueOf(p.getProducts_qty())));
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+                cell.setPaddingRight(5);
+                table.addCell(cell);
+            }
+
+            PdfWriter.getInstance(document, out);
+            document.open();
+            document.add(table);
+
+            document.close();
+
+        } catch (DocumentException ex) {
+            System.out.println("ERROR");
+        }
+
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+}
